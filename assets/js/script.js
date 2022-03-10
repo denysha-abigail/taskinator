8a4f55b2f08a167ad035cf3229fdcda1f777dffe
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-03-10 16:06:59 -0500

    added submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 // DOM object reference
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Create a task function
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-    }
+    };
 
 // Event listener function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
